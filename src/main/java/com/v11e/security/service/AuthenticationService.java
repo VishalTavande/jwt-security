cef95bb8ec557098cfac4e2eaa8a824d39e3f2a6
VishalTavande <vrtavande@gmail.com>
2023-01-19 00:57:48 +0530

    added service

--- src/main/java/com/v11e/security/service/AuthenticationService.java
+++ src/main/java/com/v11e/security/service/AuthenticationService.java
@@ -3,17 +3,45 @@ package com.v11e.security.service;
 import com.v11e.security.dto.AuthenticationRequest;
 import com.v11e.security.dto.AuthenticationResponse;
 import com.v11e.security.dto.RegisterRequest;
+import com.v11e.security.repository.UserRepository;
+import com.v11e.security.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
-    public AuthenticationResponse register(RegisterRequest request) {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository repository;
+    private final JwtService service;
+    private final AuthenticationManager authenticationManager;
 
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .build();
+        repository.save(user);
+        var jwtToken = service.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        var jwtToken = service.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
